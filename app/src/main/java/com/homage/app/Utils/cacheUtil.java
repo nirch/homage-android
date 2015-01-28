247885da877b9432c2672ca605381ef1fc6bb16b
dangalg <dangalg@gmail.com>
2015-01-28 15:40:52 +0200

    fixed share to work everywhere!

--- app/src/main/java/com/homage/app/Utils/cacheUtil.java
+++ app/src/main/java/com/homage/app/Utils/cacheUtil.java
@@ -1,8 +1,11 @@
 package com.homage.app.Utils;
 
+import android.content.ContentResolver;
+import android.content.ContentValues;
 import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
+import android.provider.MediaStore;
 
 import java.io.File;
 
@@ -30,13 +33,21 @@ public class cacheUtil {
 
         emailIntent.putExtra(android.content.Intent.EXTRA_TEXT, body);
 
-        //Add the attachment by specifying a reference to our custom ContentProvider
-        //and the specific file of interest
-        File shareFile = new File(context.getCacheDir() + File.separator + fileName);
-//        This line makes the magic!
-        shareFile.setReadable(true, false);
+        // Add the attachment by specifying a reference to our custom ContentProvider
+        // and the specific file of interest
+//        File shareFile = new File(context.getCacheDir() + File.separator + fileName);
+        // This line makes the magic!
+//        shareFile.setReadable(true, false);
 
-        emailIntent.putExtra(Intent.EXTRA_STREAM, Uri.fromFile(shareFile));
+        ContentValues content = new ContentValues(4);
+        content.put(MediaStore.Video.VideoColumns.DATE_ADDED,
+                System.currentTimeMillis() / 1000);
+        content.put(MediaStore.Video.Media.MIME_TYPE, type);
+        content.put(MediaStore.Video.Media.DATA, context.getCacheDir() + File.separator + fileName);
+        ContentResolver resolver = context.getContentResolver();
+        Uri uri = resolver.insert(MediaStore.Video.Media.INTERNAL_CONTENT_URI, content);
+
+        emailIntent.putExtra(Intent.EXTRA_STREAM, uri);//Uri.fromFile(shareFile)
 
         return emailIntent;
     }
